bf1a10695e1bcc85fdedc9287d959b2caa25a574
Latifah Zawedde <zawedde2021@gmail.com>
2022-10-15 12:17:58 -0700

    creating userthought api files

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+router.route("/friends/:friendId")
+    .put(addFriend)
+    .delete(removeFriend)
+
+module.exports = router;
